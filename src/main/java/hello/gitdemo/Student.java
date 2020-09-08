008ccc079a194e26f5cea7ce400cb96adf7e621d
Truc19082000 <57611372+Truc19082000@users.noreply.github.com>
2020-09-08 09:17:30 +0700

    version1

---
+++ src/main/java/hello/gitdemo/Student.java
@@ -0,0 +1,16 @@
+package hello.gitdemo;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+    
+    
+}
